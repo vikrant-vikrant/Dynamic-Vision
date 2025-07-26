008066975268d5c121424be37d8626846b42c44a
Vikrant <vikrant2003vikrant@gmail.com>
2025-07-26 12:18:54 +0530

    add page2 and backend

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+const ejsMate = require("ejs-mate");
+app.engine("ejs", ejsMate);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/home", (req, res) => {
+  res.render("listings/index.ejs");
+});
+
+app.listen(8000, () => {
+  console.log(`App is listing to port : 8000`);
+});
